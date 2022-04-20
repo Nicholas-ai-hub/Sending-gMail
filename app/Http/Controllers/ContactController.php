22eb99e9bb5666fc07d627717a2a77d8ad4e315d
Jacob George <kooldrjay060@gmail.com>
2022-04-20 16:01:19 +0100

    sending mail fix

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -15,35 +17,24 @@ class ContactController extends Controller
 
         ]);
 
-        if($this->isOnline()){
-            return "Connected!";
-        }
-        else{
-            return "No Connection";
-        }
-    }
-
-    public function isOnline($site = "https://youtube.com",Request $request)
-    {
-        if(@fopen($site, "r")){
+        // try {
             $mail_data = [
-                'recipient' => '',//your email
-                'fromEmail' => $request->email,
-                'fromName' => $request->name,
+                'sender' => $request->email,
+                'name' => $request->name,
                 'subject' => $request->subject,
                 'body' => $request->message,
             ];
 
-            \Mail::send('email-template',$mail_data, function($message) use ($mail_data){
-                $message->to($mail_data['recipient'])
-                ->from($mail_data['fromEmail'], $mail_data['fromName'])
-                ->subject($mail_data['subject']);
-            });
+            Mail::to('jacobs@datazen.online','Owner')
+            ->send(new ContactMail($mail_data));
 
             return redirect()->back()->with('success', 'Email sent');
-        }
-        else{
-            return redirect()->back()->withInput()->with('error', 'Check your internet connection');
-        }
+
+        // } catch (\Exception $e) {
+
+        //     dd($e->getMessage());
+        //     return redirect()->back()->withInput()->with('error', 'Check your internet connection');
+        // }
+        
     }
 }
